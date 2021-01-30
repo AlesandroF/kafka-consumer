a2ea97f498da89972ebecaad8712ffb7f7fc21d8
Alesandro Fidelis <BRHBS0490@ambev.com.br>
2021-01-30 14:38:01 -0300

    initial commit

---
+++ kafka-consumer/Program.cs
@@ -0,0 +1,51 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+
+namespace kafka_consumer
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConsumerConfig
+            {
+                GroupId = "test-consumer-group",
+                BootstrapServers = "localhost:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+            {
+                consumer.Subscribe("test-topic");
+
+                CancellationTokenSource cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) =>
+                {
+                    e.Cancel = true;
+                    cts.Cancel();
+                };
+
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var consume = consumer.Consume(cts.Token);
+                            Console.WriteLine($"Consumed message '{consume.Message.Value}' at '{consume.TopicPartitionOffset}'.");
+                        }
+                        catch (ConsumeException ex)
+                        {
+                            Console.WriteLine($"Error ocurred: {ex.Error.Reason}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    consumer.Close();
+                }
+            }
+        }
+    }
+}
